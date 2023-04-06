e873898de8213eaf5259d0cb646e7be06bcd574f
Juliette Cordor <juliette@jewelexx.com>
2023-04-06 21:05:36 +1000

    fix

--- src/requests.c
+++ src/requests.c
@@ -6,15 +6,19 @@
 
 #include "../include/requests.h"
 
-static FILE *fp;
+char *path;
 
 size_t write_data(void *buff, size_t size, size_t nmemb, void *userp) {
+    FILE *fp = fopen(path, "a");
+
     if (fp == NULL) {
         printf("write_data: fp is null\n");
         exit(1);
     }
 
     fwrite(buff, size, nmemb, fp);
+
+    fclose(fp);
 }
 
 void request_url(IgnoreFile *file) {
@@ -23,18 +27,14 @@ void request_url(IgnoreFile *file) {
         return;
     }
 
-    char *path = file->path;
+    path = file->path;
     if (path == NULL) {
         return;
     }
-
-    fp = fopen(path, "a");
 
     CURL *curl = curl_easy_init();
 
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
     CURLcode success = curl_easy_perform(curl);
-
-    fclose(fp);
 }
