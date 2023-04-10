9df9f8af645561ad29a20ba3b138d81eb22c2d15
Juliette Cordor <juliette@jewelexx.com>
2023-04-10 13:51:01 +1000

    fixes

--- src/requests.c
+++ src/requests.c
@@ -4,11 +4,15 @@
 
 #include "../include/requests.h"
 
-IgnoreFile *args;
 char *path;
 
-size_t write_data(void *buff, size_t size, size_t nmemb, void *userp) {
-    FILE *fp = fopen(args->path, "a");
+typedef struct {
+    IgnoreFile *args;
+    FILE *fp;
+} OutputData;
+
+size_t write_data(void *buff, size_t size, size_t nmemb, FILE *userp) {
+    FILE *fp = userp->fp;
 
     if (fp == NULL) {
         perror("failed to open file\n");
@@ -26,30 +30,42 @@ size_t write_data(void *buff, size_t size, size_t nmemb, void *userp) {
 }
 
 void request_url(IgnoreFile *file) {
-    args = file;
-    char *url = file->url;
-    if (url == NULL) {
+    if (file->url == NULL) {
         printf("request_url: url is null\n");
         return;
     }
 
-    path = file->path;
-    if (path == NULL) {
+    if (file->path == NULL) {
         printf("request_url: path is null\n");
         return;
     }
 
+    FILE *fp = fopen(file->path, "a");
+
     curl_global_cleanup();
     CURL *curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_URL, file->url);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
     CURLcode error = curl_easy_perform(curl);
+
+    int to_exit = 0;
+
+    if (fclose(fp)) {
+        perror("failed to close file");
+        to_exit = 1;
+    }
 
     if (error != CURLE_OK) {
         const char *err_desc = curl_easy_strerror(error);
         printf("%s\n", err_desc);
+        to_exit = 1;
     }
 
     curl_easy_cleanup(curl);
     curl_global_cleanup();
+
+    if (to_exit) {
+        exit(1);
+    }
 }
